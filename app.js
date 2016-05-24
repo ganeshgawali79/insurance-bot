bd8608cbaf06437bc1ebc7c5624e3698cc5c28b9
pjhagerty <pjhagerty@gmail.com>
2016-05-24 12:53:46 -0400

    Entangled in the battle between cloudant and passport

--- app.js
+++ app.js
@@ -60,11 +60,15 @@ app.listen(appEnv.port, '0.0.0.0', function () {
     });
 
     // process the login form
-    app.post('/login', passport.authenticate('local-login', {
-      successRedirect : '/home', // redirect to the secure profile section
-      failureRedirect : '/login', // redirect back to the signup page if there is an error
-      failureFlash : true // allow flash messages
-    }));
+    /* app.post('/login', passport.authenticate('local-login', {
+      successRedirect : '/home', // redirect to home once signed in
+      failureRedirect : '/login', //return to login if there is an error
+      failureFlash : true
+    })); */
+
+    app.post('/login', passport.authenticate('local',
+      { successRedirect: '/',
+        failureRedirect: '/login' }));
 
     // process the about page
     app.get("/about", function (req, res) {
